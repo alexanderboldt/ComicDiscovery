09f21133485652a6520ec79402c1004c2b7ab2ca
Alexander Boldt <alexx1216@live.de>
2021-11-15 10:18:58 +0100

    image-screen will be shown as fullscreen initially

--- app/src/main/java/com/alex/comicdiscovery/feature/image/ImageScreen.kt
+++ app/src/main/java/com/alex/comicdiscovery/feature/image/ImageScreen.kt
@@ -4,17 +4,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.alex.comicdiscovery.ui.theme.Black
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @ExperimentalCoilApi
 @Composable
 fun ImageScreen(url: String) {
+    val systemUiController = rememberSystemUiController()
+
+    LaunchedEffect(Unit) {
+        systemUiController.isSystemBarsVisible = false
+    }
+
     Image(
         painter = rememberImagePainter(url),
         contentDescription = null,
-        modifier = Modifier.background(Black).fillMaxSize()
+        modifier = Modifier
+            .background(Black)
+            .fillMaxSize()
     )
 }
