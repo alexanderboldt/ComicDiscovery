eee93b5b0e8a9dacf82eb37feed69bf642f059bf
Alexander Boldt <alexx1216@live.de>
2022-03-29 13:12:18 +0200

    deaktivated buildconfig-creation in modules

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -37,6 +37,10 @@ android {
         }
     }
 
+    buildFeatures {
+        buildConfig = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
